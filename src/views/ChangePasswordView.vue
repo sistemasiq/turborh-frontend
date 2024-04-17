35e84e44f1833efc111960838d854427770acbdf
Salvador <scasalvador.ch11@gmail.com>
2024-04-17 12:58:48 -0600

    FEAT: Restablecimiento de contraseña por parte de administradores del sistema
    -Añadida la capacidad de que los jefes puedan restablecer la contraseña.
    
    -Envío de WhatsApp únicamente para usuarios que tengan numero de teléfono, independientemente del rol que tengan.
    
    -Busqueda de imagen de los usuarios en las carpetas del bucket. Ahora dependiendo del rol que tengan se buscará en la carpeta de administradores o de usuarios externos

--- src/views/ChangePasswordView.vue
+++ src/views/ChangePasswordView.vue
@@ -1,12 +1,8 @@
 <template>
-   <q-layout view="hhr lpr lff" class="bg-blue-grey-1">
+  <q-layout view="hhr lpr lff" class="bg-blue-grey-1">
     <q-page-container>
-      <div
-        style="width: 100%; height: 150px"
-        class="row justify-center"
-      >
-        <div style="width: 100%;"
-        class="row justify-center q-mt-lg">
+      <div style="width: 100%; height: 150px" class="row justify-center">
+        <div style="width: 100%" class="row justify-center q-mt-lg">
           <q-img
             :src="getS3FileUrl(getAssetsPath, 'logo_turbo_black.png')"
             style="width: 70px; height: 70px"
@@ -16,164 +12,162 @@
           Restablecer Contraseña
         </div>
       </div>
-      <div
-        style="width: 100%;"
-        class="row justify-center"
-      >
-      <q-card
-        style="min-width: fit-content; width: 30%; border-radius: 10px"
-      >
-        <q-card-section horizontal>
-          <div class="q-pa-sm  row justify-between" style="width: 100%;">
-            <div class="text-h6 q-pa-sm">
-            Crea una nueva contraseña
-          </div>
-          <q-btn round flat icon="help" size="md" @click="helpDialog = !helpDialog" />
-          </div>
-        </q-card-section>
+      <div style="width: 100%" class="row justify-center">
+        <q-card style="min-width: fit-content; width: 30%; border-radius: 10px">
+          <q-card-section horizontal>
+            <div class="q-pa-sm row justify-between" style="width: 100%">
+              <div class="text-h6 q-pa-sm">Crea una nueva contraseña</div>
+              <q-btn
+                round
+                flat
+                icon="help"
+                size="md"
+                @click="helpDialog = !helpDialog"
+              />
+            </div>
+          </q-card-section>
 
-        <q-separator color="blue-grey-5" />
+          <q-separator color="blue-grey-5" />
 
-        <q-card-section class="q-mb-md">
-          <div class="text-grey-7 text-body1">
-            Utiliza letras y números para crear una contraseña más segura.
-          </div>
-          <q-card-actions align="center">
-            <q-input
-              outlined
-              v-model="password"
-              :type="isPasswordVisible ? 'text  ' : 'password'"
-              label="Ingresa tu nueva contraseña"
-              standout="text-green-13"
-              label-color="grey-8"
-              class="q-mt-md"
-              :rules="[
-                (value) => !!value || 'Este campo no puede estar vacío.',
-              ]"
-              style="width: 70%"
-            >
-              <template v-slot:append>
-                <q-icon
-                  :name="isPasswordVisible ? 'visibility_off' : 'visibility'"
-                  class="cursor-pointer"
-                  @click="isPasswordVisible = !isPasswordVisible"
-                />
-              </template>
-            </q-input>
+          <q-card-section class="q-mb-md">
+            <div class="text-grey-7 text-body1">
+              Utiliza letras y números para crear una contraseña más segura.
+            </div>
+            <q-card-actions align="center">
+              <q-input
+                outlined
+                v-model="password"
+                :type="isPasswordVisible ? 'text  ' : 'password'"
+                label="Ingresa tu nueva contraseña"
+                standout="text-green-13"
+                label-color="grey-8"
+                class="q-mt-md"
+                :rules="[
+                  (value) => !!value || 'Este campo no puede estar vacío.',
+                ]"
+                style="width: 70%"
+              >
+                <template v-slot:append>
+                  <q-icon
+                    :name="isPasswordVisible ? 'visibility_off' : 'visibility'"
+                    class="cursor-pointer"
+                    @click="isPasswordVisible = !isPasswordVisible"
+                  />
+                </template>
+              </q-input>
 
-            <q-input
-              outlined
-              v-model="confirmPassword"
-              :type="isPasswordVisible ? 'text  ' : 'password'"
-              label="Confirma tu contraseña"
-              standout="text-green-13"
-              label-color="grey-8"
-              :rules="[
-                (value) => !!value || 'Este campo no puede estar vacío.',
-              ]"
-              style="width: 70%"
-            >
-              <template v-slot:append>
-                <q-icon
-                  :name="isPasswordVisible ? 'visibility_off' : 'visibility'"
-                  class="cursor-pointer"
-                  @click="isPasswordVisible = !isPasswordVisible"
-                />
-              </template>
-            </q-input>
-          </q-card-actions>
-          <div class="row text-grey-8">
-            Nivel de seguridad de la contraseña:
-            <q-badge
-            class="q-ml-xs"
-            :label="evaluateSecurityLevel"
-              :color="
-                evaluateSecurityLevel === 'Bajo'
-                  ? 'red-13'
-                  : evaluateSecurityLevel === 'Buena'
-                  ? 'amber-14'
-                  : evaluateSecurityLevel === 'Optima'
-                  ? 'green-13'
-                  : 'grey'
-              "
-            />
-          </div>
-        </q-card-section>
-
-        <q-separator color="blue-grey-5" />
-
-        <q-card-section>
-          <q-card-actions class="justify-end">
-            <q-btn
-              flat
-              label="Cancelar"
-              class="bg-grey-4 text-grey-8 q-mr-sm"
-              style="border-radius: 8px"
-              @click="toLogin()"
-            />
-            <q-btn
-              flat
-              label="Confirmar"
-              class="bg-green-13 text-white"
-              style="border-radius: 8px"
-              @click.prevent="changePassword()"
-              :disable="disableButton()"
-            />
-          </q-card-actions>
-        </q-card-section>
-      </q-card>
-      </div>
-      <q-dialog
-      v-model="helpDialog"
-      >
-        <q-card
-        class="fixed-center"
-        style="min-width: fit-content; width: 30%; border-radius: 10px"
-      >
-      <q-card-section horizontal>
-          <div class="q-pa-sm  row justify-between items-center" style="width: 100%;">
-            <div class="text-h6 q-pa-sm">
-              Crear una contraseña segura
-          </div>
-          <q-btn style="width: fit-content; height: fit-content;" round flat icon="close" size="md" @click="helpDialog = !helpDialog" />
-          </div>
-        </q-card-section>
-
-        <q-separator color="blue-grey-5" />
-
-        <q-card-section class="q-mb-md">
-
-          <div class="text-grey-7 text-body1">
-
-          </div>
-          <div class="row text-grey-8">
-            Sigue las siguientes recomendaciones para crear una contraseña
-            <q-badge
-            class="q-ml-xs"
-            label="Optima"
-              color="green-13"
-            />
-          </div>
-          <div class="q-ml-md q-mt-sm">
+              <q-input
+                outlined
+                v-model="confirmPassword"
+                :type="isPasswordVisible ? 'text  ' : 'password'"
+                label="Confirma tu contraseña"
+                standout="text-green-13"
+                label-color="grey-8"
+                :rules="[
+                  (value) => !!value || 'Este campo no puede estar vacío.',
+                ]"
+                style="width: 70%"
+              >
+                <template v-slot:append>
+                  <q-icon
+                    :name="isPasswordVisible ? 'visibility_off' : 'visibility'"
+                    class="cursor-pointer"
+                    @click="isPasswordVisible = !isPasswordVisible"
+                  />
+                </template>
+              </q-input>
+            </q-card-actions>
             <div class="row text-grey-8">
-            <q-icon name="arrow_right" color="black" size="xs"/>
-            No uses información personal (e. nombres, apodos, etc.)
-          </div>
-          <div class="row text-grey-8">
-            <q-icon name="arrow_right" color="black" size="xs"/>
-            Usa simbolos y números (e. * . - 4 , etc)
-          </div>
-          <div class="row text-grey-8">
-            <q-icon name="arrow_right" color="black" size="xs"/>
-            Combina mayusculas y minusculas
-          </div>
-          <div class="row text-grey-8">
-            <q-icon name="arrow_right" color="black" size="xs"/>
-            Escribe mínimo 6 caracteres
-          </div>
-          </div>
-        </q-card-section>
-      </q-card>
+              Nivel de seguridad de la contraseña:
+              <q-badge
+                class="q-ml-xs"
+                :label="evaluateSecurityLevel"
+                :color="
+                  evaluateSecurityLevel === 'Bajo'
+                    ? 'red-13'
+                    : evaluateSecurityLevel === 'Buena'
+                    ? 'amber-14'
+                    : evaluateSecurityLevel === 'Optima'
+                    ? 'green-13'
+                    : 'grey'
+                "
+              />
+            </div>
+          </q-card-section>
+
+          <q-separator color="blue-grey-5" />
+
+          <q-card-section>
+            <q-card-actions class="justify-end">
+              <q-btn
+                flat
+                label="Cancelar"
+                class="bg-grey-4 text-grey-8 q-mr-sm"
+                style="border-radius: 8px"
+                @click="toLogin()"
+              />
+              <q-btn
+                flat
+                label="Confirmar"
+                class="bg-green-13 text-white"
+                style="border-radius: 8px"
+                @click.prevent="changePassword()"
+                :disable="disableButton()"
+              />
+            </q-card-actions>
+          </q-card-section>
+        </q-card>
+      </div>
+      <q-dialog v-model="helpDialog">
+        <q-card
+          class="fixed-center"
+          style="min-width: fit-content; width: 30%; border-radius: 10px"
+        >
+          <q-card-section horizontal>
+            <div
+              class="q-pa-sm row justify-between items-center"
+              style="width: 100%"
+            >
+              <div class="text-h6 q-pa-sm">Crear una contraseña segura</div>
+              <q-btn
+                style="width: fit-content; height: fit-content"
+                round
+                flat
+                icon="close"
+                size="md"
+                @click="helpDialog = !helpDialog"
+              />
+            </div>
+          </q-card-section>
+
+          <q-separator color="blue-grey-5" />
+
+          <q-card-section class="q-mb-md">
+            <div class="text-grey-7 text-body1"></div>
+            <div class="row text-grey-8">
+              Sigue las siguientes recomendaciones para crear una contraseña
+              <q-badge class="q-ml-xs" label="Optima" color="green-13" />
+            </div>
+            <div class="q-ml-md q-mt-sm">
+              <div class="row text-grey-8">
+                <q-icon name="arrow_right" color="black" size="xs" />
+                No uses información personal (e. nombres, apodos, etc.)
+              </div>
+              <div class="row text-grey-8">
+                <q-icon name="arrow_right" color="black" size="xs" />
+                Usa simbolos y números (e. * . - 4 , etc)
+              </div>
+              <div class="row text-grey-8">
+                <q-icon name="arrow_right" color="black" size="xs" />
+                Combina mayusculas y minusculas
+              </div>
+              <div class="row text-grey-8">
+                <q-icon name="arrow_right" color="black" size="xs" />
+                Escribe mínimo 6 caracteres
+              </div>
+            </div>
+          </q-card-section>
+        </q-card>
       </q-dialog>
     </q-page-container>
   </q-layout>
@@ -183,21 +177,28 @@
 import { ref, computed } from "vue";
 import axios from "axios";
 import { storeToRefs } from "pinia";
-import {
-  passwordChange,
-  sendEmail,
-} from "src/services/mail.js";
+import { passwordChange, sendEmail } from "src/services/mail.js";
 import { notifyPositive, notifyNegative } from "src/utils/notifies.js";
 import { userCredentials } from "src/utils/operations.js";
 import { useAuthStore } from "src/stores/auth.js";
 import { useRouter } from "vue-router";
 import { useQuasar } from "quasar";
-import { sendPasswordChangedInformation, passwordChangedInformation } from "src/services/whatsApp.js";
+import {
+  sendPasswordChangedInformation,
+  passwordChangedInformation,
+} from "src/services/whatsApp.js";
 import { getS3FileUrl } from "src/services/profiles.js";
 import { getAssetsPath } from "src/utils/folderPaths";
 
 const useAuth = useAuthStore();
-const { userId, userName, userEmail, userPhoneNumber, photoUUID, verificationCode } = storeToRefs(useAuth);
+const {
+  userId,
+  userName,
+  userEmail,
+  userPhoneNumber,
+  photoUUID,
+  verificationCode,
+} = storeToRefs(useAuth);
 const $q = useQuasar();
 const router = useRouter();
 const password = ref("");
@@ -225,19 +226,38 @@ const changePassword = async () => {
       const request = await axios.put(url, data);
 
       if (request.status == 200) {
-        const mailData = passwordChange(userEmail.value, userName.value);
+        if (userEmail.value != "" && userPhoneNumber.value != null) {
+          const mailData = passwordChange(userEmail.value, userName.value);
 
-        sendPasswordChangedInformation(passwordChangedInformation(userPhoneNumber.value, "reclutamiento@turbomaquinas.com"));
-        sendEmail("password-changed-confirmation", mailData);
-        $q.notify(notifyPositive("Contraseña actualizada"));
-        toPasswordRestored();
+          sendPasswordChangedInformation(
+            passwordChangedInformation(
+              userPhoneNumber.value,
+              "reclutamiento@turbomaquinas.com"
+            )
+          );
+          sendEmail("password-changed-confirmation", mailData);
+          $q.notify(notifyPositive("Contraseña actualizada"));
+          toPasswordRestored();
 
-        userId.value = '';
-        userName.value = '';
-        userEmail.value = '';
-        photoUUID.value = '';
-        verificationCode.value = '';
-        password.value = '';
+          userId.value = "";
+          userName.value = "";
+          userEmail.value = "";
+          photoUUID.value = "";
+          verificationCode.value = "";
+          password.value = "";
+        } else if ((userEmail.value != "", userPhoneNumber.value == null)) {
+          const mailData = passwordChange(userEmail.value, userName.value);
+          sendEmail("password-changed-confirmation", mailData);
+          $q.notify(notifyPositive("Contraseña actualizada"));
+          toPasswordRestored();
+
+          userId.value = "";
+          userName.value = "";
+          userEmail.value = "";
+          photoUUID.value = "";
+          verificationCode.value = "";
+          password.value = "";
+        }
       }
     } catch (error) {
       console.log("ERROR while changing the password: " + error);
