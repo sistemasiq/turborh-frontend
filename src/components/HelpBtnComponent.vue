f735bf7394b202cce4bd67257a018129d9d7482d
Salvador Cervantes <90328294+stormxcoder11@users.noreply.github.com>
2024-11-25 17:02:05 -0600

    FEAT: Botón de ayuda al usuario
    El usuario podrá ver un botón en el inicio de sesión, donde al presionarlo podrán ver un dialogo en pantalla sugiriendo enviar sus dudas y comentarios a un correo

---
+++ src/components/HelpBtnComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-btn
+    icon="help"
+    label="¿Necesitas ayuda?"
+    class="text-dark bg-white"
+    @click="emitClick"
+  />
+</template>
+
+<script setup>
+
+const emit = defineEmits(["click"]);
+
+const emitClick = () => {
+  emit("click");
+};
+</script>
